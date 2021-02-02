f77c141a970a74d892747f6015ca2449e5d1b59d
Urs97 <64044473+Urs97@users.noreply.github.com>
2021-02-02 20:37:11 +0100

    fixed missing en link in documents cms

--- src/components/Documents/Documents.js
+++ src/components/Documents/Documents.js
@@ -19,7 +19,7 @@ const Documents = () => {
                     <SectionHeader title={documentsTitle} subtitle={documentsSubtitle} icon={'file-alt'} />
                     <section className={styles.documents}>
                         {documents.map((doc, index) => (
-                            <a key={index} href={doc.link} target="_blank" rel="noopener noreferrer" className={styles.link}>{lang === 'en' ? doc.name_en : doc.name_hr}</a> 
+                            <a key={index} href={lang === 'en' ? doc.link_en : doc.link_hr} target="_blank" rel="noopener noreferrer" className={styles.link}>{lang === 'en' ? doc.name_en : doc.name_hr}</a> 
                         ))}
                     </section>
                 </section>
