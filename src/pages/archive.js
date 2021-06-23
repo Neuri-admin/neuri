d299a431b19381aa6db069681c8e58ba2aaaf91c
Urs97 <64044473+Urs97@users.noreply.github.com>
2021-06-23 20:06:31 +0200

    added SEO component to archive and arhiva pages

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import SEO from "../components/seo"
 import Archive from '../components/Archive/Archive'
 
 const ArchivePage = () => {
     return (
         <>
+            <SEO         
+                title={"NeuRi 2021 Archive"}
+            />
             <Archive />
         </>
     )
--- src/pages/arhiva.js
+++ src/pages/arhiva.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import SEO from "../components/seo"
 import Archive from '../components/Archive/Archive'
 
 const ArhivaPage = () => {
     return (
         <>
+            <SEO         
+                title={"NeuRi 2021 Arhiva"}
+            />
             <Archive />
         </>
     )
